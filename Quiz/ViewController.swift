312729d72abe68170ad10f038ed5a3746fa5c4d7
pdrotrs <patlleon@aol.com>
2016-02-24 18:57:48 -0700

    1st commit

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -7,19 +7,51 @@
 //
 
 import UIKit
+struct Question {
+    var Question : String!
+    var Answers : [String]!
+    var Answer : Int!
+}
 
 class ViewController: UIViewController {
 
+    @IBOutlet var Buttons: [UIButton]!
+    @IBOutlet var QLabel: UILabel!
+    
+    var Questions = [Question]()
+    
+    var QNumber = Int()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        Questions = [Question(Question: "Would you use your phone while having a date?", Answers: ["Yes!","No!","I don't have a date"], Answer: 2)]
+        
+        PickQuestion()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func PickQuestion() {
+    
+        if Questions.count > 0 {
+            QNumber = 0
+            QLabel.text = Questions[QNumber].Question
+            
+            for i in 0..<Buttons.count {
+                Buttons[i].setTitle(Questions[QNumber].Answers[i], forState: UIControlState.Normal)
+            }
+            
+            Questions.removeAtIndex(QNumber)
+        }
+        
+        else {
+            NSLog("Done!")
+        }
+    }
 }
 
